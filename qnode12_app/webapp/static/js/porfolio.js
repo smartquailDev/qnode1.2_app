9311fc8854a7c4cb33613dc6bc709dc4efffe741
smartquailDev <smartquail.dev@gmail.com>
2022-06-07 19:33:19 -0500

    inicials

--- qnode12_app/webapp/static/js/porfolio.js
+++ qnode12_app/webapp/static/js/porfolio.js
@@ -1,39 +1,23 @@
-$(function() {
-    $(".img-w").each(function() {
-      $(this).wrap("<div class='img-c'></div>")
-      let imgSrc = $(this).find("img").attr("src");
-       $(this).css('background-image', 'url(' + imgSrc + ')');
-    })
-              
-    
-    $(".img-c").click(function() {
-      let w = $(this).outerWidth()
-      let h = $(this).outerHeight()
-      let x = $(this).offset().left
-      let y = $(this).offset().top
-      
-      
-      $(".active").not($(this)).remove()
-      let copy = $(this).clone();
-      copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active")
-      $(".active").css('top', y - 8);
-      $(".active").css('left', x - 8);
-      
-        setTimeout(function() {
-      copy.addClass("positioned")
-    }, 0)
-      
-    }) 
-    
-    
-  
-    
-  })
-  
-  $(document).on("click", ".img-c.active", function() {
-    let copy = $(this)
-    copy.removeClass("positioned active").addClass("postactive")
-    setTimeout(function() {
-      copy.remove();
-    }, 500)
-  })
+        // $('.portfolio-item').isotope({
+        //  	itemSelector: '.item',
+        //  	layoutMode: 'fitRows'
+        //  });
+        $('.portfolio-menu ul li').click(function(){
+          $('.portfolio-menu ul li').removeClass('active');
+          $(this).addClass('active');
+          
+          var selector = $(this).attr('data-filter');
+          $('.portfolio-item').isotope({
+            filter:selector
+          });
+          return  false;
+        });
+        $(document).ready(function() {
+        var popup_btn = $('.popup-btn');
+        popup_btn.magnificPopup({
+        type : 'image',
+        gallery : {
+          enabled : true
+        }
+        });
+        });
