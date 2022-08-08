4782d2d225860025f14684b495b7deb2c2d22235
smartquailDev <smartquail.dev@gmail.com>
2022-08-07 23:04:47 -0500

    update

--- qnode12_app/shop/models.py
+++ qnode12_app/shop/models.py
@@ -32,16 +32,16 @@ class Category(TranslatableModel):
 
 class Product(TranslatableModel):
     translations = TranslatedFields( 
-    category = models.ForeignKey(Category,
-                                 related_name='products',
-                                 on_delete=models.CASCADE),
+    
     name = models.CharField(max_length=200, db_index=True),
     slug = models.SlugField(max_length=200, db_index=True),
     description = models.TextField(blank=True),
     price = models.DecimalField(max_digits=10, decimal_places=2)
     )
     available = models.BooleanField(default=True)
-    
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE,null=True)
 
     item1 = models.CharField(max_length=200,null=True)
     item2 = models.CharField(max_length=200,null=True)
