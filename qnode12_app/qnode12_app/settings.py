cdcaff92765ef07ab5a5afd3f92f75124f944c1c
smartquailDev <smartquail.dev@gmail.com>
2022-08-23 13:41:04 -0500

    update

--- qnode12_app/qnode12_app/settings.py
+++ qnode12_app/qnode12_app/settings.py
@@ -111,9 +111,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'wagtail.contrib.redirects.middleware.RedirectMiddleware',
-    
-    
+    'wagtail.contrib.redirects.middleware.RedirectMiddleware', 
 ]
 
 ROOT_URLCONF = 'qnode12_app.urls'
