0606817e6d3c99febfa56215d682009c8b1cae1c
smartquailDev <smartquail.dev@gmail.com>
2022-10-07 01:52:36 -0500

    update

--- qnode12_app/shop/views.py
+++ qnode12_app/shop/views.py
@@ -20,6 +20,22 @@ def product_list(request, category_slug=None):
                    'categories': categories,
                    'products': products})
 
+def cart_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        language = request.LANGUAGE_CODE
+        category = get_object_or_404(Category,
+                                     translations__language_code=language,
+                                     translations__slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/cart_list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
 
 def product_detail(request, id, slug,category_slug=None):
     category = None
