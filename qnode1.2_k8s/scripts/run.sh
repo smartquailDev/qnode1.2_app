830d3044cac416dd41d7827f9a48b4c44768a80c
smartquailDev <smartquail.dev@gmail.com>
2022-10-08 03:06:12 -0500

    update

--- qnode1.2_k8s/scripts/run.sh
+++ qnode1.2_k8s/scripts/run.sh
@@ -4,9 +4,7 @@ set -e
 APP_PORT=${PORT:-9000}
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"smartquail.info@gmail.com"}
 
-go get github.com/mailhog/mhsendmail
-cp /root/go/bin/mhsendmail /usr/bin/mhsendmail
-echo 'sendmail_path = /usr/bin/mhsendmail --smtp-addr mailhog:1025' > /usr/local/etc/php/php.ini
+
 python manage.py migrate --noinput
 python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
 python manage.py collectstatic --noinput 
